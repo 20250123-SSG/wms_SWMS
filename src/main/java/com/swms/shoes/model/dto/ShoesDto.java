e8ffe85e25fdff036e08c3b8ce54e51582dfa08b
YoungGaLee <rkduddl1717@gmail.com>
2025-03-31 23:40:28 +0900

    feat(dto): add ShoesDto

--- src/main/java/com/swms/shoes/model/dto/ShoesDto.java
+++ src/main/java/com/swms/shoes/model/dto/ShoesDto.java
@@ -1,4 +1,19 @@
 package com.swms.shoes.model.dto;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
 public class ShoesDto {
+    private int shoesCode;
+    private int brandNo;
+    private int typeNo;
+    private String category;
+    private String shoesName;
+    private Integer price;
+    private Integer size;
 }
