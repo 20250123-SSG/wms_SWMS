f3a357613264b7b471de3a03b4824eb2cf70c0a7
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-04-06 00:55:26 +0900

    feat: 창고(온/오프) 리스트 조회

--- src/main/java/com/swms/warehouse/model/dao/OfflineWarehouseMapper.java
+++ src/main/java/com/swms/warehouse/model/dao/OfflineWarehouseMapper.java
@@ -1,4 +1,16 @@
 package com.swms.warehouse.model.dao;
 
+
+
+import com.swms.warehouse.model.dto.OfflineWarehouseDto;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface OfflineWarehouseMapper {
+
+    List<OfflineWarehouseDto> selectAllOfflineWarehouse(
+            @Param("limit") int limit,
+            @Param("offset") int offset
+    );
 }
