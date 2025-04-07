373d8a320a703709a053c8a8a8f7c12be0930980
kimsh0313 <swe6000@naver.com>
2025-04-07 12:01:58 +0900

    feat: 신발 등록 수정 삭제 완료
    .

--- src/main/java/com/swms/shoes/controller/ShoesManagementController.java
+++ src/main/java/com/swms/shoes/controller/ShoesManagementController.java
@@ -7,10 +7,9 @@ import com.swms.user.view.ResultView;
 import java.util.Map;
 
 public class ShoesManagementController {
-    private ShoesManagementService admintService = new ShoesManagementService();
+    private ShoesManagementService shoesManagementService = new ShoesManagementService();
 
     public void registShoes(Map<String, String> requestParam) {
-        // 요청시 전달된 데이터 => MenuDto 옮기기
         ShoesDto menu = ShoesDto.builder()
                 .shoesId(Integer.valueOf(requestParam.get("id")))
                 .shoesTypeId(Integer.valueOf(requestParam.get("typeid")))
@@ -20,7 +19,7 @@ public class ShoesManagementController {
                 .size(Integer.parseInt(requestParam.get("size")))
                 .build();
 
-        int result = admintService.registShoes(menu);
+        int result = shoesManagementService.registShoes(menu);
         ResultView.displayResult("신발 등록", result);
 
     }
@@ -28,19 +27,17 @@ public class ShoesManagementController {
     public void modifyShoes(Map<String, String> requestParam) {
         ShoesDto menu = ShoesDto.builder()
                 .shoesId(Integer.valueOf(requestParam.get("id")))
-                .shoesTypeId(Integer.valueOf(requestParam.get("typeid")))
-                .brandId(Integer.valueOf(requestParam.get("brand")))
                 .shoesName(requestParam.get("name"))
                 .shoesPrice(requestParam.get("price"))
                 .size(Integer.parseInt(requestParam.get("size")))
                 .build();
 
-        int result = admintService.modifyShoes(menu);
+        int result = shoesManagementService.modifyShoes(menu);
         ResultView.displayResult("신발 수정", result);
     }
     public void removeShoes(String id) {
 
-        int result = admintService.removeShoes( Integer.parseInt(id) );
+        int result = shoesManagementService.removeShoes( Integer.parseInt(id) );
         ResultView.displayResult("신발 삭제", result);
 
     }
--- src/main/java/com/swms/shoes/view/ShoesManagementView.java
+++ src/main/java/com/swms/shoes/view/ShoesManagementView.java
@@ -37,7 +37,7 @@ public class ShoesManagementView {
         return sc.nextLine();
 
     }
-    // 신규 메뉴 등록용 폼 제공 화면
+    // 신규 신발 등록용 폼 제공 화면
     public void registShoesForm(){
         System.out.println("\n~~~ 등록할 신발 정보를 작성해주세요 ~~~");
         System.out.println("> 신발ID: ");
@@ -65,15 +65,11 @@ public class ShoesManagementView {
         shoesManagementController.registShoes(requestParam);
     }
 
-    // 기존 메뉴 수정용 폼 제공 화면
+    // 기존 신발 수정용 폼 제공 화면
     private void modifyShoesForm() {
         System.out.println("\n~~~~ 수정할 신발 정보를 입력해주세요 ~~~");
-        System.out.println("> 신발ID: ");
+        System.out.println("> 수정할 신발ID: ");
         String shoesId = sc.nextLine();
-        System.out.println("> 신발타입ID: ");
-        String shoesTypeId = sc.nextLine();
-        System.out.println("> 브랜드ID: ");
-        String brandId = sc.nextLine();
         System.out.println("> 신발명: ");
         String shoesName = sc.nextLine();
         System.out.println("> 신발 가격: ");
@@ -83,8 +79,6 @@ public class ShoesManagementView {
 
         Map<String, String> requestParam = Map.of(
                 "id", shoesId,
-                "typeid", shoesTypeId,
-                "brand", brandId,
                 "name", shoesName,
                 "price", shoesPrice,
                 "size", size
