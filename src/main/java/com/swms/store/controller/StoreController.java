802573782f51d278667c24a2025e32d5610bbdec
InYong Jung <83904581+iysh321@users.noreply.github.com>
2025-04-07 10:52:31 +0900

    feat: 유저(점장)에 맞는 매장 정보 불러오기

--- src/main/java/com/swms/store/controller/StoreController.java
+++ src/main/java/com/swms/store/controller/StoreController.java
@@ -1,4 +1,15 @@
 package com.swms.store.controller;
 
+import com.swms.store.model.dto.StoreDto;
+import com.swms.store.model.service.StoreService;
+import com.swms.user.model.dto.AccountDto;
+
 public class StoreController {
+
+    private StoreService storeService = new StoreService();
+
+    public StoreDto getStoreByUserId(int userId) {
+
+        return storeService.getStoreByUserId(userId);
+    }
 }
--- src/main/java/com/swms/store/model/service/StoreService.java
+++ src/main/java/com/swms/store/model/service/StoreService.java
@@ -1,4 +1,21 @@
 package com.swms.store.model.service;
 
+import com.swms.store.model.dao.StoreMapper;
+import com.swms.store.model.dto.StoreDto;
+import org.apache.ibatis.session.SqlSession;
+
+import static com.swms.common.Template.getSqlSession;
+
 public class StoreService {
+
+    private StoreMapper storeMapper;
+
+    public StoreDto getStoreByUserId(int userId) {
+        SqlSession sqlSession = getSqlSession();
+        storeMapper = sqlSession.getMapper(StoreMapper.class);
+
+        sqlSession.close();
+
+        return storeMapper.findByUserId(userId);
+    }
 }
